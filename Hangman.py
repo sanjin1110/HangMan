a775dbf475d5d2483d15e839d313c809922870b6
AmshSHrestha <sthaamsh37@gmail.com>
2021-09-10 10:10:59 +0545

    Created words and basic step for hangman

--- Hangman.py
+++ Hangman.py
@@ -3,6 +3,10 @@ from PIL import ImageTk,Image
 import random
 import sys
 
+wordlist=["Nuwakot","Kathmandu", "Bhaktapur", "Chitwan", "Pokhara"]
+lives=6
+score=0
+
 win=Tk()
 win.title("Hangman")
 win.geometry("800x500")
