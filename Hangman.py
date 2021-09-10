42c06477561ce77601f8670b55f310f547922119
AmshSHrestha <sthaamsh37@gmail.com>
2021-09-10 10:07:49 +0545

    Created Tkinter window

---
+++ Hangman.py
@@ -0,0 +1,11 @@
+from tkinter import*
+from PIL import ImageTk,Image
+import random
+import sys
+
+win=Tk()
+win.title("Hangman")
+win.geometry("800x500")
+win.iconbitmap("Hangman.ico")
+
+win.mainloop()
